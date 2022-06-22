8af10915312fc1a9d70e838f96b085dcdd513a51
DianaDob <dobsinszki.diana@gmail.com>
2022-06-22 16:42:59 +0200

    feat: create CRUD methods

--- src/main/java/com/codecool/pyp/controller/PlantController.java
+++ src/main/java/com/codecool/pyp/controller/PlantController.java
@@ -1,7 +1,41 @@
 package com.codecool.pyp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.pyp.model.Plant;
+import com.codecool.pyp.service.PlantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class PlantController {
+
+    @Autowired
+    private PlantService plantService;
+
+    @GetMapping("/api/plants")
+    private List<Plant> getAllPlants(){
+        return plantService.getAllPlants();
+    }
+
+    @GetMapping("/api/plants/{id}")
+    private Plant getPlant(@PathVariable ("id") UUID id){
+        return plantService.getPlant(id);
+    }
+
+    @DeleteMapping("/api/plants/{id}")
+    private void deletePlant(@PathVariable ("id") UUID id){
+        plantService.deletePlant(id);
+    }
+
+    @PostMapping("/api/plants")
+    private void addPlant(@RequestBody Plant plant){
+        plantService.addOrUpdatePlant(plant);
+    }
+
+    @PutMapping("api/plants")
+    private void updatePlant(@RequestBody Plant plant){
+        plantService.addOrUpdatePlant(plant);
+    }
 }
---
+++ src/main/java/com/codecool/pyp/service/PlantService.java
@@ -0,0 +1,24 @@
+package com.codecool.pyp.service;
+
+import com.codecool.pyp.model.Plant;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PlantService {
+    public List<Plant> getAllPlants() {
+        return null;
+    }
+
+    public Plant getPlant(UUID id) {
+        return null;
+    }
+
+    public void deletePlant(UUID id) {
+    }
+
+    public void addOrUpdatePlant(Plant plant) {
+    }
+}
