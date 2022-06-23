59e90320acd7a620d2f087e3b21836c311c786d5
eniko42 <szaboeniko1220@gmail.com>
2022-06-23 09:41:02 +0200

    refac: change uuid to int in database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS comment;
 CREATE TABLE comment
 (
-    id         UUID      NOT NULL PRIMARY KEY,
-    plant_id   UUID      NOT NULL,
+    id         int      NOT NULL PRIMARY KEY,
+    plant_id   int      NOT NULL,
     message    VARCHAR   NOT NULL,
     user_name  VARCHAR   NOT NULL,
     time_stamp TIMESTAMP NOT NULL
@@ -11,7 +11,7 @@ CREATE TABLE comment
 DROP TABLE IF EXISTS plant;
 CREATE TABLE plant
 (
-    id          UUID PRIMARY KEY,
+    id          int PRIMARY KEY,
     user_name   VARCHAR NOT NULL,
     plant_name  VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
@@ -25,8 +25,8 @@ ALTER TABLE comment
     ADD FOREIGN KEY (plant_id) REFERENCES plant (id);
 
 INSERT INTO plant
-VALUES (RANDOM_UUID(), 'Dia', 'Krókusz', 'Kedves tavaszi virág', 'plant1.jpg', 'Budapest IV. kerület', 'krokusz@krokusz.com', true);
+VALUES (1, 'Dia', 'Krókusz', 'Kedves tavaszi virág', 'plant1.jpg', 'Budapest IV. kerület', 'krokusz@krokusz.com', true);
 INSERT INTO plant
-VALUES (RANDOM_UUID(), 'Eszter', 'Anyósnyelv', 'zöld', 'plant1.jpg', 'Gödöllő', 'eszter@eszter.com', true);
-INSERT INTO comment VALUES (RANDOM_UUID(), '7faceeca-d4c5-41b7-90e6-f5916da87a06', 'Hali ez nekem nagyon tetszik', 'Enikő', CURRENT_TIMESTAMP());
---INSERT INTO comment VALUES ( RANDOM_UUID(), 2, 'Uhhh de szép ez nekem kell', 'Berni', CURRENT_TIMESTAMP() )
+VALUES (1, 'Eszter', 'Anyósnyelv', 'zöld', 'plant1.jpg', 'Gödöllő', 'eszter@eszter.com', true);
+INSERT INTO comment VALUES (1, 1, 'Hali ez nekem nagyon tetszik', 'Enikő', CURRENT_TIMESTAMP());
+INSERT INTO comment VALUES (2, 2, 'Uhhh de szép ez nekem kell', 'Berni', CURRENT_TIMESTAMP() )
