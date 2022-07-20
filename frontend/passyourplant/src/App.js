b7c21a3b703f9997067ed1f021ab136b4866d0a6
eniko42 <szaboeniko1220@gmail.com>
2022-07-20 19:44:20 +0200

    feat: change plant name to link

--- frontend/passyourplant/src/App.js
+++ frontend/passyourplant/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './App.css';
-import Navbar from'./Navbar';
+import Navbar from './Navbar';
 import { Title } from './Title';
 import { Footer } from './Footer';
-import {Card} from './Card';
+import { Card } from './Card';
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -11,48 +11,47 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-        title: "Pass Your Plant",
-        plants: [],
-        DataisLoaded: false
+      title: "Pass Your Plant",
+      plants: [],
+      DataisLoaded: false
     };
-}
+  }
 
-componentDidMount() {
-  fetch("/api/plants")
+  componentDidMount() {
+    fetch("/api/plants")
       .then((res) => res.json())
       .then((json) => {
-          this.setState({
-              plants: json,
-              DataisLoaded: true
-          });
-          //console.log(this.state.plants);
+        this.setState({
+          plants: json,
+          DataisLoaded: true
+        });
       })
-}
+  }
   render() {
-    const { DataisLoaded, plants} = this.state;
-    if(!DataisLoaded) return <div>
+    const { DataisLoaded, plants } = this.state;
+    if (!DataisLoaded) return <div>
       <h1>Please wait...</h1>
     </div>
-  return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
+    return (
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
 
-        <Title title={this.state.title}></Title>
-        
-      </header>
-      <div className="Card-container">
-      {this.state.plants.map((plant, idx) => (
-          <Card key={idx} name={plant.plant_name} pic={plant.photo} available={plant.available}/>
-      ))}
+          <Title title={this.state.title}></Title>
+
+        </header>
+        <div className="Card-container">
+          {this.state.plants.map((plant, idx) => (
+            <Card key={idx} plant={plant} name={plant.plant_name} />
+          ))}
+        </div>
+
+        <Footer />
+
+
       </div>
-      
-      <Footer />
-    
-      
-    </div>
-  );
-      }
+    );
+  }
 }
 
 export default App;
