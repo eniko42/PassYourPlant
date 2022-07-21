0ccd1b79c47d95b92a2bdcf11fb17e47cd6307d7
DianaDob <dobsinszki.diana@gmail.com>
2022-07-21 15:25:56 +0200

    feat: implement fetch POST new plant

--- frontend/passyourplant/src/AddPlant.js
+++ frontend/passyourplant/src/AddPlant.js
@@ -2,33 +2,57 @@ import React from "react"
 import "./AddPlant.css"
 
 class AddPlant extends React.Component{
+
+
+handleClick(){
+    const userName = document.getElementById("user_name").value;
+    const plantName = document.getElementById("plant_name").value;
+    const desc = document.getElementById("desc").value;
+    const loc = document.getElementById("loc").value;
+    const contact = document.getElementById("contact").value;
+    console.log(userName);
+    fetch('api/plants', {
+        method: "POST",
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            "user_name": userName,
+            "plant_name": plantName,
+            "description": desc,
+            "photo": "test_plant.jpg",
+            "location": loc,
+            "contact": contact,
+            "available": true    
+           })
+    })
+}
+
     render(){
         return(
         <div>
             <h1>Upload a plant you want to give away</h1>
             <form className="detailsCard">
                 <div className="formItem">
-                    <label for="user_name">Your name: </label>
+                    <label htmlFor="user_name">Your name: </label>
                     <input type="text" name="user_name" id="user_name" required></input>
                 </div>
                 <div className="formItem">
-                    <label for="plant_name">Plant name: </label>
+                    <label htmlFor="plant_name">Plant name: </label>
                     <input type="text" name="plant_name" id="plant_name" required></input>
                 </div>
                 <div className="formItem">
-                    <label for="desc">Description: </label>
+                    <label htmlFor="desc">Description: </label>
                     <input type="text" name="desc" id="desc"></input>  
                 </div>
                 <div className="formItem">
-                    <label for="loc">Location: </label>             
+                    <label htmlFor="loc">Location: </label>             
                     <input type="text" name="loc" id="loc" required></input>
                 </div>
                 <div className="formItem">
-                    <label for="contact">Contact info: </label>
+                    <label htmlFor="contact">Contact info: </label>
                     <input type="text" name="contact" id="contact"></input>
                 </div>                            
                 
-                <button className="submitButton" type="submit">Upload</button>     
+                <button onClick={this.handleClick} className="submitButton" type="submit">Upload</button>     
 
             </form>
 
