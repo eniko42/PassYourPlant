01beea9a00155cc0fa196e21cc0f85adf77aae21
eniko42 <szaboeniko1220@gmail.com>
2022-08-02 11:27:13 +0200

    feat: change to Jpa repository

--- backend/src/main/java/com/codecool/pyp/storage/CommentRepository.java
+++ backend/src/main/java/com/codecool/pyp/storage/CommentRepository.java
@@ -1,8 +1,9 @@
 package com.codecool.pyp.storage;
 
 import com.codecool.pyp.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 }
--- backend/src/main/java/com/codecool/pyp/storage/PlantRepository.java
+++ backend/src/main/java/com/codecool/pyp/storage/PlantRepository.java
@@ -1,7 +1,8 @@
 package com.codecool.pyp.storage;
 
 import com.codecool.pyp.model.Plant;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PlantRepository extends CrudRepository<Plant, Integer> {
+public interface PlantRepository extends JpaRepository<Plant, Long> {
 }
