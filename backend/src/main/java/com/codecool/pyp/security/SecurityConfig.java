fbdcc67fbaf7c642295d90e74b7df5e7257a1c07
eniko42 <szaboeniko1220@gmail.com>
2022-08-16 11:33:55 +0200

    feat: add password encoder

--- backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static java.lang.String.format;
@@ -38,5 +40,10 @@ public class SecurityConfig{
                         )
                 );
         return auth;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
