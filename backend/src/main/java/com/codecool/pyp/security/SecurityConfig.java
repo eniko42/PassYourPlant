af6c14e26cd5b3a798a1b77cef912878a95b88b0
DianaDob <dobsinszki.diana@gmail.com>
2022-08-16 11:27:50 +0200

    feat: create SecurityConfig

---
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.codecool.pyp.security;
+
+import com.codecool.pyp.storage.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static java.lang.String.format;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        return null;
+    }
+
+    @Bean
+    AuthenticationManagerBuilder configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> userRepository
+                .findByName(username)
+                        .orElseThrow(
+                                () -> new UsernameNotFoundException(
+                                        format("User: %s not found", username)
+                                )
+                        )
+                );
+        return auth;
+    }
+}
