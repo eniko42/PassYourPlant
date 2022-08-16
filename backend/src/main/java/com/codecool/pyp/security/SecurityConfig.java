2745e161d3a8fa3f064889e1679a6d810906b975
eniko42 <szaboeniko1220@gmail.com>
2022-08-16 12:30:17 +0200

    feat: implement the filterchain

--- backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -3,13 +3,21 @@ package com.codecool.pyp.security;
 import com.codecool.pyp.storage.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 import static java.lang.String.format;
 
@@ -18,15 +26,55 @@ import static java.lang.String.format;
 public class SecurityConfig{
 
     private final UserRepository userRepository;
+    private final JwtTokenFilter jwtTokenFilter;
 
-    public SecurityConfig(UserRepository userRepository) {
+    public SecurityConfig(UserRepository userRepository, JwtTokenFilter jwtTokenFilter) {
         this.userRepository = userRepository;
+        this.jwtTokenFilter = jwtTokenFilter;
     }
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http){
-        return null;
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http = http.cors().and().csrf().disable();
+
+        // Set session management to stateless
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        // Set unauthorized requests exception handler
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            response.sendError(
+                                    HttpServletResponse.SC_UNAUTHORIZED,
+                                    ex.getMessage()
+                            );
+                        }
+                )
+                .and();
+
+        // Set permissions on endpoints
+        http.authorizeRequests()
+                // Our public endpoints
+                .antMatchers("/api/public/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/book/search").permitAll()
+                // Our private endpoints
+                .anyRequest().authenticated();
+
+        // Add JWT token filter
+        http.addFilterBefore(
+                jwtTokenFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+        return http.build();
     }
 
     @Bean
