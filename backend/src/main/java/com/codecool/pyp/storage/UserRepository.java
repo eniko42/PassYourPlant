49746e068464a87d6a1580e010ba64046c16ef15
DianaDob <dobsinszki.diana@gmail.com>
2022-08-16 11:30:46 +0200

    feat: add method findByName to UserRepository, returning Optional<UserDetails>

--- backend/src/main/java/com/codecool/pyp/storage/UserRepository.java
+++ backend/src/main/java/com/codecool/pyp/storage/UserRepository.java
@@ -2,8 +2,13 @@ package com.codecool.pyp.storage;
 
 import com.codecool.pyp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findByName(String username);
 }
