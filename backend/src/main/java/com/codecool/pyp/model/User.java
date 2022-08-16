6ccd20a83b4fbfad3f4e348c48a9e515c9e44457
DianaDob <dobsinszki.diana@gmail.com>
2022-08-16 11:28:59 +0200

    feat: make User implement UserDetails with methods and fields

--- backend/src/main/java/com/codecool/pyp/model/User.java
+++ backend/src/main/java/com/codecool/pyp/model/User.java
@@ -1,15 +1,22 @@
 package com.codecool.pyp.model;
 
 import javax.persistence.*;
-import lombok.*;
 
+import com.codecool.pyp.security.Role;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @Table(name="users")
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -28,4 +35,39 @@ public class User {
 
     @OneToMany
     private Set<Comment> comments;
+
+    @ElementCollection
+    private Set<Role> authorities;
+
+    private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
