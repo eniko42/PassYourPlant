3d97d2b9ee7a1387d0fa385a0af0940a4baa759d
eniko42 <szaboeniko1220@gmail.com>
2022-08-16 12:30:51 +0200

    feat: implement corsfilter method

--- backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -94,4 +94,17 @@ public class SecurityConfig{
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
