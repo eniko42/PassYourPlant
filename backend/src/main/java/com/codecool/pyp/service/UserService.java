be0a011fde252dd263b9149fd403beb3455cdb50
DianaDob <dobsinszki.diana@gmail.com>
2022-08-17 13:50:40 +0200

    feat: create UserService implementing UserDetailsService

---
+++ backend/src/main/java/com/codecool/pyp/service/UserService.java
@@ -0,0 +1,25 @@
+package com.codecool.pyp.service;
+
+import com.codecool.pyp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByName(username).orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", username)));
+    }
+}
