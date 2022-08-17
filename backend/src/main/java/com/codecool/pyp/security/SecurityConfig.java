86f17a539c54ca2d599b712f89b7643f5c00b2f4
DianaDob <dobsinszki.diana@gmail.com>
2022-08-17 13:53:58 +0200

    fix: autowire UserService and change implementation of configure(AuthenticationManagerBuilder auth)

--- backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.codecool.pyp.security;
 
 import com.codecool.pyp.repository.UserRepository;
+import com.codecool.pyp.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -31,11 +33,11 @@ import static java.lang.String.format;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final JwtTokenFilter jwtTokenFilter;
 
-    public SecurityConfig(UserRepository userRepository, JwtTokenFilter jwtTokenFilter) {
-        this.userRepository = userRepository;
+    public SecurityConfig(UserService userService, JwtTokenFilter jwtTokenFilter) {
+        this.userService= userService;
         this.jwtTokenFilter = jwtTokenFilter;
     }
 
@@ -77,15 +79,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> userRepository
-                .findByName(username)
-                        .orElseThrow(
-                                () -> new UsernameNotFoundException(
-                                        format("User: %s not found", username)
-                                )
-                        )
-                );
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userService);
+        return provider;
     }
 
     @Bean
