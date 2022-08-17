7133ea3183cccb0f9d7e811bc82728e72c27a6c5
DianaDob <dobsinszki.diana@gmail.com>
2022-08-17 13:23:32 +0200

    fix: update import statement of UserRepository in SecurityConfig

--- backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.codecool.pyp.security;
 
-import com.codecool.pyp.storage.UserRepository;
+import com.codecool.pyp.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
