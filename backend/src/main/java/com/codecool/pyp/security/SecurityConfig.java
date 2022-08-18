097da5115e394c92b27141633274b346e2f8ed12
eniko42 <szaboeniko1220@gmail.com>
2022-08-18 10:24:42 +0200

    feat: make api/plants route available without authentication

--- backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
+++ backend/src/main/java/com/codecool/pyp/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Set permissions on endpoints
         http.authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*", "/webjars/**/*").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/plants/*", "api/plants/*/comments").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/plants/*", "/api/plants", "api/plants/*/comments").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/plants", "api/plants/*/comments").hasRole(USER.name())
                 .antMatchers(HttpMethod.PUT, "/api/plants", "api/plants/*/comments").hasRole(USER.name())
                 .antMatchers(HttpMethod.DELETE, "/api/plants/*", "/api/plants/comments/*").hasRole(USER.name())
