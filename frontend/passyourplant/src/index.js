16c6f7fc28c99d06e4257fa43c7c38da1ff2c66e
eniko42 <szaboeniko1220@gmail.com>
2022-08-18 13:49:46 +0200

    feat: add logout route

--- frontend/passyourplant/src/index.js
+++ frontend/passyourplant/src/index.js
@@ -4,25 +4,27 @@ import './style/index.css';
 import App from './components/App';
 import reportWebVitals from './components/reportWebVitals';
 import {
-  BrowserRouter,
-  Routes,
-  Route,
+    BrowserRouter,
+    Routes,
+    Route,
 } from "react-router-dom";
 import About from './components/About';
 import PlantDetail from './components/PlantDetail';
 import {AddPlant} from './components/AddPlant';
 import Layout from './components/Layout';
+import Login from './components/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout><App /></Layout>} />
-      <Route path="about" element={<Layout><About /></Layout> }/>
-      <Route path='plant-detail/:plantId' element={<Layout><PlantDetail /></Layout>} />
-      <Route path="add-plant" element={<Layout><AddPlant/></Layout>}/>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout><App/></Layout>}/>
+            <Route path="about" element={<Layout><About/></Layout>}/>
+            <Route path='plant-detail/:plantId' element={<Layout><PlantDetail/></Layout>}/>
+            <Route path="add-plant" element={<Layout><AddPlant/></Layout>}/>
+            <Route path='/login' element={<Layout><Login /></Layout>}/>
+        </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
