3bc9cf11f7371c2e16804f163e4d3fa6ef911b7e
eniko42 <szaboeniko1220@gmail.com>
2022-08-19 14:31:56 +0200

    feat: handle user name exist exception

--- backend/src/main/java/com/codecool/pyp/service/UserService.java
+++ backend/src/main/java/com/codecool/pyp/service/UserService.java
@@ -1,5 +1,6 @@
 package com.codecool.pyp.service;
 
+import com.codecool.pyp.exception.UserNameExistsException;
 import com.codecool.pyp.model.User;
 import com.codecool.pyp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -27,8 +31,12 @@ public class UserService implements UserDetailsService {
         return userRepository.findByName(username).orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", username)));
     }
 
-    public void addUser(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
+    public void addUser(User user) {
+        if (userRepository.findByName(user.getName()).isEmpty()) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+        } else {
+            throw new UserNameExistsException(user.getName() + " username already exist");
+        }
     }
 }
